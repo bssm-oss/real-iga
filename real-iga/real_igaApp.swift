3e669c884d3554187bc7c7eed4ad43f93adeaa04
leegunwoo <leegunwoo0325@gmail.com>
2026-04-09 10:59:52 +0900

    Initial Commit

---
+++ real-iga/real_igaApp.swift
@@ -0,0 +1,17 @@
+//
+//  real_igaApp.swift
+//  real-iga
+//
+//  Created by 이건우 on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct real_igaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
